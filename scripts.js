d925cc81c9e586f69759314c22c52ff4a3f5d109
Chad Root <chadmroot@gmail.com>
2022-04-11 20:10:20 +0800

    body content - sample videos

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
+var contain = document.querySelector(".contain");
 
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
+  contain.classList.toggle("large-contain");
 }

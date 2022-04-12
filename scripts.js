92b126cb5d9a2739d00f1768eaff8a5255d40e54
Chad Root <chadmroot@gmail.com>
2022-04-12 15:16:37 +0800

    added working search bar

--- scripts.js
+++ scripts.js
@@ -2,7 +2,15 @@ var menuIcon = document.querySelector(".menu-icon");
 var sidebar = document.querySelector(".sidebar");
 var contain = document.querySelector(".contain");
 
+// Change container width based on sidebar size
 menuIcon.onclick = function(){
   sidebar.classList.toggle("small-sidebar");
   contain.classList.toggle("large-contain");
 }
+
+// Search field submit
+$(document).ready(function(){
+    $("#search-submit").click(function(){        
+        $("#search-form").submit();
+    });
+});
